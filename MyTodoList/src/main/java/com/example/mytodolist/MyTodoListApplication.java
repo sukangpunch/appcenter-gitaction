d354c75d04b552cdbcfe13dcc9854b6facd0bfe3
hyeangjun <popora99@naver.com>
2023-11-21 02:44:14 +0900

    서버 url 설정

--- MyTodoList/src/main/java/com/example/mytodolist/MyTodoListApplication.java
+++ MyTodoList/src/main/java/com/example/mytodolist/MyTodoListApplication.java
@@ -1,9 +1,12 @@
 package com.example.mytodolist;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @EnableJpaAuditing
 @SpringBootApplication
 public class MyTodoListApplication {
