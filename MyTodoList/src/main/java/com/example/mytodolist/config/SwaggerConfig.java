658c404a44b58ba2d9ed1107923003192f321e2b
hyeangjun <popora99@naver.com>
2023-11-22 20:48:39 +0900

    데이터베이스 명 수정

--- MyTodoList/src/main/java/com/example/mytodolist/config/SwaggerConfig.java
+++ MyTodoList/src/main/java/com/example/mytodolist/config/SwaggerConfig.java
@@ -14,11 +14,13 @@ import springfox.documentation.spring.web.plugins.Docket;
 import java.util.Collections;
 
 @Configuration
-@EnableOpenApi
+@EnableOpenApi  // 이게 스웨거 3.0 이상버전의 최신 Swagger 어노테이션
 public class SwaggerConfig {
     @Bean
     public Docket api(){
+        //로컬에 해당하는 서버 url 설정. 배포할때 당장 사용 할 이유는 없어보인다.
         Server serverLocal = new Server("local", "http://localhost:8080/", "for local usages", Collections.emptyList(), Collections.emptyList());
+        //배포하고 Swagger를 사용 할 때 정확한 경로로 CRUD 요청을 해야 데이터베이스에 정상적으로 데이터가 입력된다.
         Server testServer = new Server("test", "http://hyeongjun.na2ru2.me/", "for testing", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.OAS_30)
                 .servers(serverLocal, testServer)
--- MyTodoList/src/main/java/com/example/mytodolist/config/Workaround.java
+++ MyTodoList/src/main/java/com/example/mytodolist/config/Workaround.java
@@ -11,25 +11,29 @@ import springfox.documentation.spi.DocumentationType;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
+
+//OpenAPI(Swagger) 문서를 변환하는 필터 역할을 하는 클래스
 @Component
 public class Workaround implements WebMvcOpenApiTransformationFilter {
 
-    @Override
+    @Override //부모 클래스나 인터페이스의 메서드를 재정의 한다는 뜻, 여기서는 transform 메서드를 재정의 하였다.
     public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
-        OpenAPI openApi = context.getSpecification();
-        Server localServer = new Server();
-        localServer.setDescription("local");
-        localServer.setUrl("http://localhost:8080/");
+        OpenAPI openApi = context.getSpecification(); //OpenApi객체를 매개변수로 받아온 현재 OpenApi 문서로 초기화
+        Server localServer = new Server(); //Server 객체 생성
+        localServer.setDescription("local"); //설명을 local로 설정
+        localServer.setUrl("http://localhost:8080/"); //url을 해당 값으로 설정
 
-        Server testServer = new Server();
-        testServer.setDescription("test");
-        testServer.setUrl("http://hyeongjun.na2ru2.me/");
-        openApi.setServers(Arrays.asList(localServer, testServer));
+        Server testServer = new Server();//Server 객체 생성
+        testServer.setDescription("test");//설명을 test로 설정
+        testServer.setUrl("http://hyeongjun.na2ru2.me/");//url을 해당 값으로 설정
+        openApi.setServers(Arrays.asList(localServer, testServer)); //OpenAPI 문서의 서버 목록을 localServer와 , testServer로 설정
         return openApi;
     }
 
-    @Override
+
+    @Override //부모 클래스나 인터페이스의 메서드를 재정의 한다는 뜻, 여기서는 support 메서드를 재정의 하였다.
     public boolean supports(DocumentationType documentationType) {
+        //우리가 사용하는 OpenAPI 문서가 Swagger 3.0 이므로, Swagger3.0 일때만 이 클래스가 실행된다.
         return documentationType.equals(DocumentationType.OAS_30);
     }
 }
